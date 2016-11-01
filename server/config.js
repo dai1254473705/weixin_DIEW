ea04ccb89c60143d9206e99ed60614f16c16c485
DIEW <1254473705@qq.com>
2016-11-01 21:33:12 +0800

    token

---
+++ server/config.js
@@ -0,0 +1,15 @@
+/**
+ * 微信认证后的 app 信息
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的数据
+ */
+module.exports = function(){
+	return {
+		//AppID(应用ID)
+		appid: '',
+		//AppSecret(应用密钥)
+		appsecret: '',
+		//Token(令牌)
+		token: 'Tokenjs'
+	};
+};
