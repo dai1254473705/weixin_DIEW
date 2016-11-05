fc5a0de992fd98580661a058f41bd61fb0cfce73
DIEW <1254473705@qq.com>
2016-11-05 14:57:05 +0800

    地理位置

--- static/test.js
+++ static/test.js
@@ -41,7 +41,7 @@ $(function(){
                 wx.onMenuShareTimeline(obj);
         });
 	},"json");
-	
+	//查看网络状态
 	$("#getNetworkType").on("click",function(){
 		wx.getNetworkType({
 		    success: function (res) {
@@ -51,4 +51,17 @@ $(function(){
 		    }
 		});
 	});
+	//查看地理位置
+	$("#getMap").on("click",function(){
+		wx.getLocation({
+	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+	    success: function (res) {
+	        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+	        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+	        var speed = res.speed; // 速度，以米/每秒计
+	        var accuracy = res.accuracy; // 位置精度
+	    }
+	})
+	
+});
 })
