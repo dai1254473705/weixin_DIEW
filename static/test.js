7c9e9b29bc2f15aee759d0e25b759267076bb651
DIEW <1254473705@qq.com>
2016-11-05 15:10:32 +0800

    text

--- static/test.js
+++ static/test.js
@@ -60,6 +60,13 @@ $(function(){
 	        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 	        var speed = res.speed; // 速度，以米/每秒计
 	        var accuracy = res.accuracy; // 位置精度
+	        var text = [
+	        	"纬度": + latitude,
+	        	"经度": + longitude,
+	        	"速度": + speed,
+	        	"位置精度": + accuracy
+	        ]
+	        console.log(text);
 	    }
 	})
 	
